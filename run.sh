151a536d0bf74077fe7fddb509e5ece0c4480b28
Junwei Zhao <wahyd4@gmail.com>
2020-05-05 20:12:50 +1000

    Fix decode command call

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 : "${KUBE_CONFIG_DATA?Must be specified}"
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
-echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
+echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 
 export KUBECONFIG=/tmp/config
 
